bcc10395f045b39f4c04f4d9e66aea5491ddf378
harupy <hkawamura0130@gmail.com>
2019-03-06 14:04:37 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# databricks-notebook-enhancer
-A Chrome extension to enhance the editor functionality of Databricks notebook
+# Databricks Notebook Enhancer
+
+Just a little hack to enhance the editor functionality of Databricks notebook.
+
+<kbd>
+  <img src="demo.gif" style="thin solid black;">
+</kbd>
+
+
+## Installation
+1. Clone this repository
+2. Open `chrome://extensions` on Chrome
+3. Enable "Developer mode"
+4. Click "Load unpacked"
+5. Select the folder that was just cloned
+
+## Usage
+The extension will be enabled automatically when you open a Databricks notebook.
+
+## Test
+1. Open a Databricks notebook
+2. Select a cell and enter the edit mode
+3. Type `df.gb`
+4. Press `Tab` (`gb` will be expanded to `groupBy()`)
+5. Press `Ctrl-u` (The current line will be duplicated below)
+6. Press `jj` (A blank line will be inserted below)
+
+
+## How it works
+What this extension does is quite simple. It just runs a JS script and overrides the properties of a `CodeMirror` object in each cell on the notebook.
+
+[CodeMirror: User Manual](https://codemirror.net/doc/manual.html)
+
+## Shortcuts
+**Note that some default shortcuts in Chrome are overriden.**
+
+|Shortcut|Action|
+|:-|:-|
+|`Ctrl-k`|Delete the word the cursor is on|
+|`Ctrl-o`|Open a blank line below|
+|`Shift-Ctrl-o`|Open a blank line above|
+|`Ctrl-l`|Delete up to the end of the current line|
+|`Ctrl-h`|Delete up to the start of the current line|
+|`Ctrl-u`|Duplicate the current line below|
+|`Shift-Ctrl-u`|Duplicate the current line above|
+
+## Snippets (Press `Tab` to expand)
+|Prefix|Snippet|
+|:-|:-|
+|`sel`|`select()`|
+|`al`|`alias()`|
+|`dt`|`distinct()`|
+|`gb`|`groupBy()`|
+|`pb`|`partitionBy()`|
+|`ps`|`printSchema()`|
+|`fl`|`filter()`|
+|`srt`|`spark.read.table()`|
+|`srp`|`spark.read.parquet()`|
+|`fft`|`from pyspark.sql import functions as f, types as t`|
+|`cnt`|`count()`|
+|`rn`|`round()`|
+|`fna`|`fillna()`|
+|`dcnt`|`distinctCount()`|
+|`btw`|`between()`|
+|`wc`|`withColumn()`|
+|`wcr`|`withColumnRenamed()`|
+|`dp`|`display()`|
+|`jn`|`join()`|
+|`tpd`|`toPandas()`|
+
+## JJ Keys
+This feature is like mapping `jj` to `Esc` in Vim. Typing these keys fast in a row triggers the action.
+
+|Keys|Action|
+|:-|:-|
+|`jj`|Open a blank like below|
+|`jk`|Go to the end of the current line|
+|`kk`|Delete the word the cursor is on|
+
+## References
+- [Is there a way to use Vim keybindings in Google Colaboratory?](https://stackoverflow.com/questions/48674326/is-there-a-way-to-use-vim-keybindings-in-google-colaboratory)
---
+++ main.js
@@ -0,0 +1,141 @@
+(() => {
+  const enhanceCell = event => {
+  // cell in editing mode
+    const cellEditing = document.querySelector('div.is-editing div.CodeMirror')
+    
+    if (cellEditing) {
+      const duplicateLineBelow = cm => {
+        const {line, ch} = cm.getCursor();
+        const currentLineContent = cm.getLine(line);
+        ['goLineRight', 'openLine', 'goLineDown'].forEach(cmd => cm.execCommand(cmd));
+        cm.replaceSelection(currentLineContent);
+        cm.setCursor({line: line + 1, ch});
+      }
+
+      const duplicateLineAbove = cm => {
+        const {line, ch} = cm.getCursor();
+        const currentLineContent = cm.getLine(line);
+        ['goLineLeft', 'newlineAndIndent', 'goLineUp'].forEach(cmd => cm.execCommand(cmd));
+        cm.replaceSelection(currentLineContent);
+        cm.setCursor({line, ch});
+      }
+
+      const deleteCursorWord = cm => {
+        const anchor = cm.getCursor();
+        const head = {line: anchor.line, ch: anchor.ch - 1};
+        const charCursorLeft = cm.getRange(head, anchor)
+        if ((anchor.ch !== 0) && ([' ', ',', '.', '(', '[', '{', '=', '~'].indexOf(charCursorLeft) === -1)) {
+          cm.execCommand('goWordLeft');
+        }
+        cm.execCommand('delWordAfter');
+      }
+
+      // snippets
+      const tabDefaultFunc = cellEditing.CodeMirror.options.extraKeys['Tab'];
+      const expandSnippetOrIndent = cm => {
+        const cursor = cm.getCursor();
+        const cursorLine= cm.getLine(cursor.line);
+        const cursorLeft = cursorLine.slice(0, cursor.ch);
+        const lastIdxs = [' ', ',', '.', '(', '[', '{', '=', '~'].map(c => cursorLeft.lastIndexOf(c));
+        const prefixStartIdx = Math.max(...lastIdxs) + 1;
+        const head = {line: cursor.line, ch: prefixStartIdx};
+        const prefix = cm.getRange(head, cursor);
+
+        const snippets = {
+          'sel'   : 'select()',
+          'al'    : 'alias()',
+          'gb'    : 'groupBy()',
+          'pb'    : 'partitionBy()',
+          'fl'   : 'filter()',
+          'srt'   : 'spark.read.table()',
+          'srp'   : 'spark.read.parquet()',
+          'fft'   : 'from pyspark.sql import functions as f, types as t',
+          'cnt'   : 'count()',
+          'rn'    : 'round()',
+          'fna'   : 'fillna()',
+          'dcnt'  : 'distinctCount()',
+          'btw'   : 'between()',
+          'wc'    : 'withColumn()',
+          'wcr'   : 'withColumnRenamed()',
+          'dp'    : 'display()',
+          'jn'    : 'join()',
+          'ps'    : 'printSchema()',
+          'sh'    : 'show()',
+          'dt'    : 'distinct()',
+          'tpd'    : 'toPandas()',
+        };
+
+        if (prefix in snippets) {
+          const body = snippets[prefix];
+          cm.replaceRange(body, head, cursor);
+          if (body.endsWith(')')) {
+            cm.moveH(-1, 'char');
+          }
+        } else {
+          tabDefaultFunc(cm);
+        }
+      }
+
+      // shortcuts
+      const extraKeyActions = {
+        'Ctrl-O'      : ['goLineRight', 'newlineAndIndent'],  // insert a new line below
+        'Shift-Ctrl-O': ['goLineLeft', 'newlineAndIndent', 'goLineUp', 'indentAuto'], // insert a new line above
+        'Ctrl-L'      : ['delWrappedLineRight'],  // kill line to end
+        'Ctrl-H'      : ['delWrappedLineLeft', 'indentAuto'],  // kill line to beginning
+        'Ctrl-K'      : [deleteCursorWord],  // delete inner word
+        'Ctrl-U'      : [duplicateLineBelow],
+        'Shift-Ctrl-U': [duplicateLineAbove],
+        'Tab'         : [expandSnippetOrIndent],
+      }
+
+      const execAction = (cm, act) => {
+        switch (typeof(act)) {
+          case 'string':
+            cm.execCommand(act);
+            break;
+          case 'function':
+            act(cm);
+            break;
+          default:
+            throw new TypeError(`Expected string or function, but got ${typeof(act)}`);
+        }
+      }
+
+      for (const [key, actions] of Object.entries(extraKeyActions)) {
+        cellEditing.CodeMirror.options.extraKeys[key] = cm => {
+          actions.forEach(act => execAction(cm, act));
+        };
+      }
+
+      // vim-like keybindings
+      const onKeyup = (cm, e) => {
+        const anchor = cm.getCursor();
+        const head = {line: anchor.line, ch:anchor.ch - 2};
+        const now = (new Date()).getTime();
+        const lapseTime = now - (cm.changedAt || now);  // unit: milliseconds 
+        cm.changedAt = now;
+        
+        const fastKeysActions = {
+          'jj': ['goLineRight', 'newlineAndIndent'],
+          'jk': ['goLineRight'],
+          'kk': [deleteCursorWord],
+        }
+
+        if (lapseTime < 500) {
+          const keys = cm.getRange(head, anchor);
+          if (keys in fastKeysActions) {
+            cm.replaceRange('', head, anchor);
+            fastKeysActions[keys].forEach(act => execAction(cm, act));
+          }
+        }
+      }
+
+      if (cellEditing.CodeMirror._handlers.keyup.length === 1) {
+        cellEditing.CodeMirror.on('keyup', onKeyup)
+      }
+    }
+  };
+
+  document.addEventListener('mouseup', enhanceCell, false);
+  document.addEventListener('keyup', enhanceCell, false);
+})();
